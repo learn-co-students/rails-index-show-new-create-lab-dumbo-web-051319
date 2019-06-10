370d3a9746c8bb6b17e7c9be780adcb33e71ff11
Rachel R <rrrachelrath@gmail.com>
2019-06-10 16:07:58 -0400

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,47 @@
+class CouponsController < ApplicationController
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+    @coupon = Coupon.new
+  end
+
+  def create
+    @coupon = Coupon.create(coupon_params)
+    redirect_to @coupon
+  end
+
+  def edit
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def update
+    @coupon = Coupon.find(params[:id])
+    @coupon.update(coupon_params)
+    redirect_to @coupon
+  end
+
+  def destroy
+    @coupon = Coupon.find(params[:id])
+    @coupon.destroy
+    redirect_to coupons_path
+  end
+
+
+
+  private
+
+  def coupon_params
+    params.require(:coupon).permit(:coupon_code, :store)
+  end
+
+
+
+
+end
